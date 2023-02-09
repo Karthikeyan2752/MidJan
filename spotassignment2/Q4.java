82199738604eaed7c34f1c38b6d078da3270d8e3
Karthikeyan M <113222543+Karthikeyan2752@users.noreply.github.com>
2023-02-09 18:45:52 +0530

    Add files via upload

--- spotassignment2/Q4.java
+++ spotassignment2/Q4.java
@@ -1,5 +1,7 @@
 package com.spotassignment2;
 
+import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 public class Q4 {
@@ -7,24 +9,38 @@ public class Q4 {
 	public static String password(Set<String> strs, String s) {
 		String result = "";
 
-		boolean[] dp = new boolean[s.length() + 1];
-		dp[0] = true;
 		for (int i = 0; i <= s.length(); i++) {
 			for (int j = 0; j < i; j++) {
-				if (dp[j] && strs.contains(s.substring(j, i))) {
-					dp[i] = true;
-					result += s.substring(j, i);
-					break;
+				if (strs.contains(s.substring(j, i))) {
+					result += s.substring(j, i) + " ";
 				}
 			}
 		}
-		return result;
+
+		String r = result.replaceAll(" ", "");
+		return r.length() == s.length() ? result : "wrong password";
+
 	}
 
 	public static void main(String[] args) {
-		Set<String> strs = Set.of( "because", "can", "do", "must", "what" );
-		String s = "wedowhatwemustbecausewecan";
-		System.out.println(password(strs,s));
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the number of words: ");
+		int n = sc.nextInt();
+
+		Set<String> strs = new HashSet<>();
+		String s = "";
+		for (int i = 0; i < n; i++) {
+			s = sc.next();
+			strs.add(s);
+		}
+
+		System.out.println("Enter the string: ");
+		s = sc.next();
+
+
+
+		System.out.println(password(strs, s));
+
 
 	}
 
